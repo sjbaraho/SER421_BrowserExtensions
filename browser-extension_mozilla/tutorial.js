c0caf3d9e5179e38451b47422db2b5a121df5fcf
Jennifer Strong <jstrongaz@gmail.com>
2022-09-20 17:59:38 -0700

    adding css to the js file

--- browser-extension_mozilla/tutorial.js
+++ browser-extension_mozilla/tutorial.js
@@ -13,3 +13,14 @@ for (let i = 0; i < aTag.length; i++) {
 }
 
 document.body.style.backgroundColor = "#e6dabc";
+document.getElementsByTagName("header")[0].style.backgroundColor = "white";
+
+let inner = document.body.innerHTML;
+inner = '<div id="wrapper">' + inner + "</div>";
+document.body.innerHTML = inner;
+let div = document.getElementById("wrapper").style;
+div.margin = "auto";
+div.width = "80%";
+div.backgroundColor = "#F8EFD8";
+div.border = "5px solid #AA8E4A";
+div.padding = "0px 5px";
